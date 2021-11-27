746d0eba500854be97b960e6f3128ad8a966ec65
Billy Mott <billygm@uw.edu>
2021-11-27 09:39:50 -0800

    adding function to record scores and save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,57 +6,64 @@ var correctEl = document.getElementById("correct");
 var q1El = document.getElementById("q1");
 var q2El = document.getElementById("q2");
 var timerEl = document.getElementById("timer");
-var timer = 60;
-
-var questions = [
-    {
-        q1El
-    },
-    {
-        q2El
-    }
-];
 var score = 0;
-var questionPointer = 1;
+var timer = 60;
+var questions = [q1El, q2El];
+var questionPointer = 0;
 var currentQuestion = questions[questionPointer];
+var nameInput = document.querySelector("#name");
+var scoreInput = document.querySelector("#score");
+var saveScoreButton = document.querySelector("#saveScore");
 
-startEl.addEventListener("click", startQuiz)
-    function startQuiz() {
-        questionsEl.textContent = currentQuestion;
-        welcomeEl.textContent = "";
-        console.log(currentQuestion);
-
-        var timeInterval = setInterval(function () {
-            if (timer >= 0) {
-                timerEl.textContent = timer;
-                timer--;
-            } else {
-                timerEl.textContent = "";
-                clearInterval(timeInterval);
-            }
-            
-        }, 1000)
-    }
-
-    function nextQuestion() {
-        questionPointer++;
-    }
-
-    function answerQuestion(event) {
-        
-        // what button was clicked
-        var buttonEl = event.target;
-        var answer = buttonEl.dataset.answer;
-
-        if(answer === currentQuestion.correct) {
-            score =+10;
-        } else if (answer !== currentQuestion.correct) {
-            timer -10;
+function startQuiz() {
+    questionsEl.textContent = currentQuestion;
+    welcomeEl.textContent = "";
+    console.log(currentQuestion);
+    
+    // the countdown timer
+    var timeInterval = setInterval(function () {
+        if (timer >= 0) {
+            timerEl.textContent = timer;
+            timer--;
+        } else {
+            questionsEl.textContent = "GAME OVER! Please record your score.";
+            timerEl.textContent = "";
+            clearInterval(timeInterval);
         }
+    }, 1000);
+};
 
-        console.log(answer)
+function nextQuestion() {
+    questionPointer++;
+};
 
-        nextQuestion();
+function answerQuestion(event) {
+    
+    // what button was clicked
+    var buttonEl = event.target;
+    var answer = buttonEl.dataset.answer;
+    
+    if(answer === currentQuestion.correct) {
+        score =+10;
+    } else if (answer !== currentQuestion.correct) {
+        timer -10;
+    }
+    
+    console.log(answer)
+    
+    nextQuestion();
+};
+
+saveScoreButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var user = {
+        name: nameInput.value.trim(),
+        score: scoreInput.value.trim(),
     }
 
+    localStorage.setItem("user", JSON.stringify(user));
+});
+
+startEl.addEventListener("click", startQuiz);
 questionsEl.addEventListener("click", answerQuestion);
