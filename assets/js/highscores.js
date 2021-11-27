465fc8505966a74b49de9efc35e2cb726dfe8a43
Billy Mott <billygm@uw.edu>
2021-11-27 13:15:31 -0800

    adding html and js for the highscores page, got my questions to display finaly

---
+++ assets/js/highscores.js
@@ -0,0 +1,7 @@
+var highscores = JSON.parse(localStorage.getItem("user")) || []
+console.log(highscores)
+highscores.forEach(highscore => {
+    var scoreEl = document.createElement("p")
+    scoreEl.innerText = highscore.name + " " + highscore.score
+    document.querySelector("#scoreboard").appendChild(scoreEl)
+})
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,50 @@
 var welcomeEl = document.getElementById("welcome");
 var questionsEl = document.getElementById("questions");
-var resultsEl = document.getElementById("results");
 var startEl = document.getElementById("start");
-var correctEl = document.getElementById("correct");
-var q1El = document.getElementById("q1");
-var q2El = document.getElementById("q2");
+var recordScore = document.querySelector("#highscores");
 var timerEl = document.getElementById("timer");
-var score = 0;
-var timer = 60;
-var questions = [q1El, q2El];
-var questionPointer = 0;
-var currentQuestion = questions[questionPointer];
 var nameInput = document.querySelector("#name");
 var scoreInput = document.querySelector("#score");
 var saveScoreButton = document.querySelector("#saveScore");
+var highscores = JSON.parse(localStorage.getItem("score")) || [];
+var score = 0;
+var timer = 5;
+var questions = [
+    {
+        question: "What's the foundation to any webpage?",
+        choices: ["CSS", "HTML", "JavaScript", "Bootstrap"],
+        correctanswer: "HTML"
+    },
+    {
+        question: "What does HTML stand for?",
+        choices: ["Hyper-Text Markup Language", "How To Meet Ladies", "How To Make Lasagna", "Hyperlinks and Text Messaging Language"],
+        correctanswer: "Hyper-Text Markup Language"
+    },
+    {
+        question: "What does CSS stand for",
+        choices: ["Captain Sloppy Soup", "Content Style Selectors", "Correcting Styles and Sentances", "Cascading Style Sheet"],
+        correctanswer: "Cascading Style Sheet"
+    },
+    {
+        question: "A 'true' or 'false' variable is called a boolean.",
+        choices: [true, false],
+        correctanswer: true
+    },
+    {
+        question: "What's the foundation to any webpage?",
+        choices: ["CSS", "HTML", "JavaScript", "Bootstrap"],
+        correctanswer: "HTML"
+    },
+];
+var questionPointer = 0;
+var currentQuestion = questions[questionPointer];
+
+recordScore.setAttribute("style", "display:none");
 
 function startQuiz() {
-    questionsEl.textContent = currentQuestion;
     welcomeEl.textContent = "";
     console.log(currentQuestion);
-    
+    renderDisplay();
     // the countdown timer
     var timeInterval = setInterval(function () {
         if (timer >= 0) {
@@ -27,42 +52,56 @@ function startQuiz() {
             timer--;
         } else {
             questionsEl.textContent = "GAME OVER! Please record your score.";
+            recordScore.setAttribute("style", "display:show");
             timerEl.textContent = "";
             clearInterval(timeInterval);
         }
     }, 1000);
 };
 
+function renderDisplay() {
+    questionsEl.textContent = currentQuestion.question;
+    currentQuestion.choices.forEach(function (choice) {
+        var questionButton = document.createElement("button")
+        questionButton.innerText = choice
+        questionsEl.appendChild(questionButton)
+        /////////////////////////////////////////
+    })
+};
+
 function nextQuestion() {
     questionPointer++;
+    currentQuestion = questions[questionPointer];
+    renderDisplay();
 };
 
 function answerQuestion(event) {
-    
+
     // what button was clicked
     var buttonEl = event.target;
-    var answer = buttonEl.dataset.answer;
-    
-    if(answer === currentQuestion.correct) {
-        score =+10;
-    } else if (answer !== currentQuestion.correct) {
-        timer -10;
+    var answer = buttonEl.innerText;
+
+    if (answer === currentQuestion.correctanswer) {
+        score + 10;
+    } else {
+        timer - 10;
     }
-    
+
+    console.log(score)
     console.log(answer)
-    
+
     nextQuestion();
 };
 
-saveScoreButton.addEventListener("click", function(event) {
+saveScoreButton.addEventListener("click", function (event) {
     event.preventDefault();
 
     var user = {
         name: nameInput.value.trim(),
         score: scoreInput.value.trim(),
     }
-
-    localStorage.setItem("user", JSON.stringify(user));
+    highscores.push(user)
+    localStorage.setItem("user", JSON.stringify(highscores));
 });
 
 startEl.addEventListener("click", startQuiz);
