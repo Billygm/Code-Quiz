5da84f7ddf27655d54c922aaaabf1a2f62ce4fef
Billy Mott <billygm@uw.edu>
2021-11-26 18:14:19 -0800

    adding a countdown timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,62 @@
-var welcomeEl = document.getElementById('welcome');
-var questionsEl = document.getElementById('questions');
-var resultsEl = document.getElementById('results');
-var startEl = document.getElementById('start');
-var correctEl = document.getElementById('correct');
+var welcomeEl = document.getElementById("welcome");
+var questionsEl = document.getElementById("questions");
+var resultsEl = document.getElementById("results");
+var startEl = document.getElementById("start");
+var correctEl = document.getElementById("correct");
+var q1El = document.getElementById("q1");
+var q2El = document.getElementById("q2");
+var timerEl = document.getElementById("timer");
+var timer = 60;
+
 var questions = [
     {
-        
+        q1El
+    },
+    {
+        q2El
     }
 ];
 var score = 0;
-var timer = 60;
-var questionPointer = 0;
+var questionPointer = 1;
 var currentQuestion = questions[questionPointer];
 
-startEl.addEventListener('click', startQuiz)
+startEl.addEventListener("click", startQuiz)
     function startQuiz() {
         questionsEl.textContent = currentQuestion;
         welcomeEl.textContent = "";
+        console.log(currentQuestion);
+
+        var timeInterval = setInterval(function () {
+            if (timer >= 0) {
+                timerEl.textContent = timer;
+                timer--;
+            } else {
+                timerEl.textContent = "";
+                clearInterval(timeInterval);
+            }
+            
+        }, 1000)
     }
 
-function nextQuestion() {
-    questionPointer++;
-}
-
-function answerQuestion(event) {
-    
-    // what button was clicked
-    var buttonEl = event.target;
-    var answer = buttonEl.dataset.answer;
-
-    if(answer === currentQuestion.correct) {
-        score =+10;
-    } else if (answer !== currentQuestion.correct) {
-        timer -10;
+    function nextQuestion() {
+        questionPointer++;
     }
 
-    console.log(answer)
+    function answerQuestion(event) {
+        
+        // what button was clicked
+        var buttonEl = event.target;
+        var answer = buttonEl.dataset.answer;
 
-    nextQuestion();
-}
+        if(answer === currentQuestion.correct) {
+            score =+10;
+        } else if (answer !== currentQuestion.correct) {
+            timer -10;
+        }
 
-questionsEl.addEventListener('click', answerQuestion);
+        console.log(answer)
+
+        nextQuestion();
+    }
+
+questionsEl.addEventListener("click", answerQuestion);
